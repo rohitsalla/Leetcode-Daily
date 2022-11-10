32f785bf20a63b483e38f55feefb1038593102b4
rohitsalla <sallarohit1011@gmail.com>
2022-11-10 11:17:22 +0530

    Create 1047. Remove All Adjacent Duplicates In String.cpp

---
+++ 1047. Remove All Adjacent Duplicates In String.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        stack<char>st;
+        string ans;
+        for(int i=0;i<s.size();i++){
+            if(!st.empty() && st.top()==s[i]){
+                st.pop();
+            }
+            else{
+                st.push(s[i]);
+            }
+        }
+        while(!st.empty()){
+            ans = st.top()+ ans;
+            st.pop();
+        }
+        return ans;
+    }
+};
+// Time COmplexity O(n)
+
